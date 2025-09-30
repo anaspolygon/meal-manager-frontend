5cf8ba0bacf9f928052660a7763e76aa60a6e7eb
anaspolygon <al.anas@polygontech.xyz>
2025-09-30 17:57:33 +0600

    update api client

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -20,12 +20,52 @@ class ApiClient {
       method?: string;
       data?: object | null;
       headers?: Record<string, string>;
-    } = {}
-  ) {
-    const options = { method, headers: { ...headers } };
-    if (this.withAuth && this.token) {
-      options.headers["Authorization"] = `Bearer ${this.token}`;
     }
-  
+  ) {
+    const options: RequestInit = { method, headers: { ...headers } };
+    if (this.withAuth && this.token) {
+      (options.headers as Record<string, string>)[
+        "Authorization"
+      ] = `Bearer ${this.token}`;
+    }
+    if (data) {
+      if (data instanceof FormData) {
+        options.body = data;
+      } else {
+        (options.headers as Record<string, string>)["Content-Type"] =
+          "application/json";
+        options.body = JSON.stringify(data);
+      }
+    }
+    const res = await fetch(`${this.baseUrl}/${endpoint}`, options);
+    try {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      return res.status === 204 ? null : await res.json();
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("API Error:", error.message);
+      } else {
+        console.error("API Error:", error);
+      }
+      throw error;
+    }
+  }
+
+  get(endpoint: string) {
+    return this.request(endpoint, { method: "GET" });
+  }
+  post(endpoint: string, data: object | null) {
+    return this.request(endpoint, { method: "POST", data });
+  }
+  put(endpoint: string, data: object | null) {
+    return this.request(endpoint, { method: "PUT", data });
+  }
+  patch(endpoint: string, data: object | null) {
+    return this.request(endpoint, { method: "PATCH", data });
+  }
+  delete(endpoint: string) {
+    return this.request(endpoint, { method: "DELETE" });
   }
 }
